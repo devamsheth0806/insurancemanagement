5fe6a9b1881b76901fbed7e06c69fb249adbd0d6
devamsheth0806 <44667716+devamsheth0806@users.noreply.github.com>
2018-11-01 15:21:59 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php  
+		$servername = "localhost";  
+		$username = "root";  
+		$password = "";  
+		$conn = mysqli_connect($servername , $username , $password,"test") or die("unable to connect to host"); 
+?>   
---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>
+Life Insurance 
+</title>
+</head>
+<body>
+<link href = "policy/registration.css" type = "text/css" rel = "stylesheet" />    
+<div class="title">
+<h1><center>Life Insurance Corporation of VIT</center></h1>
+</div>
+<div class="links">
+<div class="subtitle">
+<h2><center>Links to Datas and registration pages</center></h2>
+</div>
+<center>
+<div class="reg1">
+<a href="agent/agent.php"> <h3>Agent Registration</h3></a>
+</div>
+
+<div class="reg2">
+<a href="client/client.php"><h3> Client Registration</h3></a>
+</div>
+
+<div class="reg3">
+<a href="policy/policy.php"><h3> Policy Registration</h3></a>
+</div>
+<div class="reg1">
+<a href="premium/premium.php"><h3> Premium Registration</h3></a>
+</div>
+
+<div class="db1">
+<a href="agent/modified1.php"><h3> Agents Data</h3></a>
+</div>
+<div class="db2">
+<a href="client/modified1.php"><h3> Customers Data</h3></a>
+</div>
+<div class="reg1">
+<a href="policy/modified1.php"><h3> Policies Data</h3></a>
+</div>
+<div class="reg1">
+<a href="premium/modified1.php"><h3> Premiums Data</h3></a>
+</div>
+
+</div>
